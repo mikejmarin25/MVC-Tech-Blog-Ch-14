77e7e6a8d2a920908a1da31f49c3e556020fb2a1
mikejmarin25 <105763252+mikejmarin25@users.noreply.github.com>
2022-09-15 23:53:42 -0400

    added node modules, having issues deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,17 @@
-// Import the Sequelize constructor from the library
-const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
-// Create connection to our database, pass in your MySQL information for username and password
-let sequelize;
+console.log(process.env)
+const Sequelize = require('sequelize');
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  });
-}
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: '3306',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
